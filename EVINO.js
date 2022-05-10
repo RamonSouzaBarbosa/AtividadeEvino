7bd285299d9266d938fe234615199d46cf2782f8
RamonSouzaBarbosa <40393679+RamonSouzaBarbosa@users.noreply.github.com>
2022-05-10 00:54:45 -0300

    Update EVINO.js

--- EVINO.js
+++ EVINO.js
@@ -1,8 +1,7 @@
-
 describe ('Corta Gotas', function() {
-  
+	
 	it('Validar quantidade de corta após editar itens', function() {
-		cy.viewport(1280,800);
+		
 		cy.visit('https://www.evino.com.br/');
 		cy.get('.Large .FormControl').type('ramon.souza-b@hotmail.com');
 		cy.get('.loginSpinner').click();
@@ -18,9 +17,12 @@ describe ('Corta Gotas', function() {
 		cy.wait(2000);
 
 		//Pesquisar item
-		cy.get('.NavigationSearch__form').type('Kit 3 Bodega Norton Elegido Malbec 2021 | R$74,90 por garrafa');
+		cy.get('.NavigationSearch__form').type('Kit 3 Patas Malbec 2021');
 		//Selecionar retorno da pesquisa
 		cy.get('.SearchProductTile > a > .Media').click();
+		//Clicar no botão "Adicionar à EvinoBOX"
+		cy.get('.BuyBox__callToAction > .sc-bdVaJa').click();
+		cy.wait(2000)
 		//Clicar no ícone da EvinoBOX
 		cy.get('.CartNavigation__icon').click();
 		cy.wait(2000);
@@ -33,7 +35,7 @@ describe ('Corta Gotas', function() {
 	})
 
 	it('Validar quantidade de corta após remover um item', function() {
-		cy.viewport(1280,800);
+		
 		cy.visit('https://www.evino.com.br/');
 		cy.get('.Large .FormControl').type('ramon.souza-b@hotmail.com');
 		cy.get('.loginSpinner').click();
@@ -42,18 +44,15 @@ describe ('Corta Gotas', function() {
 		//Clicar no ícone da EvinoBOX
 		cy.get('.CartNavigation__icon').click();
 		cy.wait(2000);
-		cy.get('.cdHfmw > .Typo--medium').then(($totalItens)=>{
-			const totalInicial = $totalItens.text();
 
-			cy.get(':nth-child(2) > .CartTile > :nth-child(1) > .cwdKQS > .CartItemContentWrapper > .kJvUHU > .hZNxQG > .sc-bdVaJa').click();
-			
-			cy.get('.cdHfmw > .Typo--medium').then(($totalItens)=>{
-				const totalFinal = $totalItens.text();
-				expect(totalInicial).not.to.eq(totalFinal)
-			})	
-		})
+		//Clicar no botão "Remover"
+		cy.get(':nth-child(1) > .CartTile > :nth-child(1) > .cwdKQS > .CartItemContentWrapper > .kJvUHU > .hZNxQG > .sc-bdVaJa').click();
+		
+		//Verificar
+		cy.wait(5000);
+		cy.get(':nth-child(2) > .CartTile').should('have.text','6un. grátis')
+		
 	})
-
 
 })
   
